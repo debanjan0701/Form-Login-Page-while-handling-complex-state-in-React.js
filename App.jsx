bc4cc2e8013ebc5d5fe6b7eef9cac7510cbf57d7
Debanjan Ghosh <93243921+debanjan0701@users.noreply.github.com>
2023-02-03 11:08:08 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     const [fullname, setFullName] = useState({
         fname: "",
         lname: "",
+        email: "",
+        phone_number: "",
     });
     // const [fullname, setFullName] = useState("");
     // const [lastName, setLastName] = useState("");
@@ -13,8 +15,10 @@ const App = () => {
     const inputEvent = (event) => {
         // setName(event.target.value);
 
-        const value = event.target.value;
-        const name = event.target.name;
+        // const value = event.target.value;
+        // const name = event.target.name;
+
+        const { value, name } = event.target;
 
 
         setFullName((preValue) => {
@@ -22,12 +26,32 @@ const App = () => {
                 return {
                     fname: value,
                     lname: preValue.lname,
+                    email: preValue.email,
+                    phone_number: preValue.phone_number,
                 };
             }
             else if (name === 'lname') {
                 return {
                     fname: preValue.fname,
                     lname: value,
+                    email: preValue.email,
+                    phone_number: preValue.phone_number,
+                };
+            }
+            else if (name === 'email') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone_number: preValue.phone_number,
+                };
+            }
+            else if (name === 'phone_number') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone_number: value,
                 };
             }
         });
@@ -50,6 +74,8 @@ const App = () => {
                 <form onSubmit={onSubmits}>
                     <div>
                         <h1>Hello {fullname.fname} {fullname.lname} </h1>
+                        <p>{fullname.email}</p>
+                        <p>{fullname.phone_number}</p>
                         <input type="text"
                             placeholder="First Name"
                             name='fname'
@@ -63,6 +89,17 @@ const App = () => {
                             onChange={inputEvent}
                             //Using the {inputEvent} same function to write all the Form input
                             value={fullname.lname} />
+                        <input type="text"
+                            placeholder="email"
+                            name='email'
+                            onChange={inputEvent}
+                            value={fullname.email} />
+                        <input type="number"
+                            placeholder="Phone Number"
+                            name='phone_number'
+                            onChange={inputEvent}
+                            value={fullname.phone_number} />
+
                         <button type="submit">Submit Me 🤡</button>
                     </div>
                 </form>
